e1527656bb450c57ab2b5ae725d6bb823ed3b840
mkaiserincomm <mkaiser@incomm.com>
2021-04-28 19:45:40 +0000

    Initial check in

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM mcr.microsoft.com/dotnet/aspnet:5.0 AS base
+WORKDIR /app
+EXPOSE 80
+ENV ASPNETCORE_URLS=http://*:80
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0 AS build
+WORKDIR /src
+COPY ["webtest1.csproj", "./"]
+RUN dotnet restore "webtest1.csproj"
+COPY . .
+WORKDIR "/src/."
+RUN dotnet build "webtest1.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "webtest1.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "webtest1.dll"]
---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Get the latest code from origin
+git pull origin master
+
+# Build the docker image
+docker build -t mgkaiser/webtest1:`git rev-parse --short HEAD` .
+
+docker tag mgkaiser/webtest1:`git rev-parse --short HEAD` mgkaiser/webtest1:latest
+
+# Push the docker image to the repository (DockerHub in this case)
+docker push mgkaiser/webtest1:`git rev-parse --short HEAD`
+
+# Execute the Helm chart
+helm upgrade --install -f ./charts/webtest1/values.yaml webtest1 ./charts/webtest1 --namespace incomm-poc 
+
+# Purge any temporary images created while creating the image
+for i in $(docker images --filter=dangling=true --format "{{.ID}}"); do docker rm $i; done
+
