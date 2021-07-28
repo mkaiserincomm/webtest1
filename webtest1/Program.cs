2d94c6c6d93299e24abe31e0690e883948e6a384
mkaiserincomm <mkaiser@incomm.com>
2021-07-28 16:35:14 +0000

    Updated logging level

--- webtest1/Program.cs
+++ webtest1/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
+using Serilog.Events;
 using Serilog.Formatting.Compact;
 
 namespace webtest1
@@ -23,13 +24,17 @@ namespace webtest1
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                {                    
                     webBuilder.UseSerilog((ctx, config) => {
                         config
                             .MinimumLevel.Information()
-                            .Enrich.FromLogContext()
-                            .WriteTo.Console(new CompactJsonFormatter());
-                    });
+                            .Enrich.FromLogContext()       
+                            .MinimumLevel.Information()
+                            .MinimumLevel.Override("Microsoft.AspNetCore.Hosting.Diagnostics", LogEventLevel.Warning)   
+                            .MinimumLevel.Override("Microsoft.AspNetCore.Routing.EndpointMiddleware", LogEventLevel.Warning)  
+                            .MinimumLevel.Override("Microsoft.AspNetCore.StaticFiles.StaticFileMiddleware", LogEventLevel.Warning)                                                                   
+                            .WriteTo.Console(new CompactJsonFormatter());                            
+                    });                    
                     webBuilder.UseStartup<Startup>();
                 });
     }
